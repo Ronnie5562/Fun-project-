155cc0bd48b47d0ae433f82cd741821daf8cbdef
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2022-12-05 22:39:53 +0000

    I created a new pattern

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let colors = ['red','orange', 'yellow', 'green','blue', 'indigo', 'violet']
+/*let colors = ['red','orange', 'yellow', 'green','blue', 'indigo', 'violet']
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -8,5 +8,20 @@ function setup() {
 function draw() {
   fill(random(colors));
   ellipse(mouseX, mouseY, 20, 20);
+}*/
+
+
+// A much more beautiful pattern 
+
+let colors = ['red','orange', 'yellow', 'green','blue', 'indigo', 'violet']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(198);
+}
+
+function draw() {
+  fill(random(colors));
+  ellipse(mouseX, mouseY, 5, 20);
 }
 
